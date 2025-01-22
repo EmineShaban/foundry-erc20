98836896f2fd26e4bfd77edfe5fa94eb9413b7e1
Emine Shaban <emineshaban@icloud.com>
2025-01-22 13:10:14 +0200

    add new file

--- src/OurToken.sol
+++ src/OurToken.sol
@@ -2,8 +2,7 @@
 pragma solidity ^0.8.18;
 
 
-remappings = ["@openzeppelin=lib/openzeppelin-contracts"]
-
+ 
 contract OurToken {
     constructor (uint256 initialSupply) ERC20("OurToken", "OT"){
         _mint(msg.sender, initialSupply);
